8f7252a959f77d1beb3925aa45477c2e1f77256e
Gabriel Erik Cruz Miranda <144914703+Gaberik@users.noreply.github.com>
2024-06-30 23:57:16 -0400

    Add files via upload

---
+++ php/conexion_form.php
@@ -0,0 +1,10 @@
+<?php
+    $conex = mysqli_connect("localhost", "root", "","formulario");
+    /*
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo 'No se pudo conectar a la Base de Datos';
+    }
+    */
+?>
